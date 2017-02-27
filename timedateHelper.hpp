818c9681c5a7b5d83a4c6622675d33f604ee647f
Jan Schöppach <jan.schoeppach@bestsens.de>
2017-02-27 10:12:40 +0100

    add timedateHelper

---
+++ timedateHelper.hpp
@@ -0,0 +1,100 @@
+/*
+ * timedateHelper.hpp
+ *
+ *  Created on: 23.02.2017
+ *      Author: Jan Schöppach
+ */
+
+#ifndef TIMEDATEHELPER_HPP_
+#define TIMEDATEHELPER_HPP_
+
+#include <string>
+#include <vector>
+#include <sstream>
+#include <memory>
+#include <regex>
+#include <iostream>
+
+namespace bestsens {
+    namespace timedateHelper {
+        std::vector<std::string> pipeSystemCommand(const std::string& command) {
+            std::string cmd = command + " 2>&1";
+            std::array<char, 128> line;
+            std::vector<std::string> lines;
+            std::shared_ptr<FILE> pipe(popen(cmd.c_str(), "r"), pclose);
+
+            if(!pipe) throw std::runtime_error("popen() failed!");
+
+            while(!feof(pipe.get())) {
+                if(fgets(line.data(), 128, pipe.get()) != NULL)
+                    lines.push_back(std::string(line.data()));
+            }
+
+            return lines;
+        }
+
+        /*
+         * SOURCE: http://stackoverflow.com/a/2275160/481329
+         */
+        std::vector<std::string> split(std::string const &input) {
+            std::istringstream buffer(input);
+            std::vector<std::string> ret((std::istream_iterator<std::string>(buffer)), std::istream_iterator<std::string>());
+            return ret;
+        }
+
+        bool hasEnding(std::string const &fullString, std::string const &ending) {
+            if (fullString.length() >= ending.length()) {
+                return (0 == fullString.compare (fullString.length() - ending.length(), ending.length(), ending));
+            } else {
+                return false;
+            }
+        }
+
+        std::string getDate() {
+            for(auto input : pipeSystemCommand("timedatectl status")) {
+                std::regex r("Local.*([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2})");
+                std::smatch match;
+
+                if(std::regex_search(input, match, r))
+                    if(match.ready() && match.size() == 2)
+                        return match[1];
+            }
+
+            throw std::runtime_error("could not get current time");
+
+            return "";
+        }
+
+        void setDate(const std::string& date) {
+            std::string cmd = std::string("sudo -n timedatectl set-time \"") + date + std::string("\"");
+
+            auto lines = pipeSystemCommand(cmd);
+
+            for(auto line : lines) {
+                std::cout << line << std::endl;
+            }
+
+            if(lines.size() > 0) {
+                std::string error = std::string("could not set timesync: ") + lines[0];
+                throw std::runtime_error(error);
+            }
+        }
+
+        void setTimesync(bool timesync_enabled) {
+            std::string cmd = std::string("sudo -n timedatectl set-timezone \"") + std::string("\"");
+
+            auto lines = pipeSystemCommand(cmd);
+
+            for(auto line : lines) {
+                std::cout << line << std::endl;
+            }
+
+            if(lines.size() > 0) {
+                std::string error = std::string("could not set timesync: ") + lines[0];
+                throw std::runtime_error(error);
+            }
+        }
+    }
+}
+
+#endif /* TIMEDATEHELPER_HPP_ */
