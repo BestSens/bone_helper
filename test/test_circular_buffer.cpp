d95c06407b1345fea394542ff83da6e59b0c3055
Jan Schöppach <jan.schoeppach@bestsens.de>
2017-08-03 10:40:47 +0200

    Add optional stress test for CircularBuffer

--- test/test_circular_buffer.cpp
+++ test/test_circular_buffer.cpp
@@ -1,6 +1,8 @@
 #include "libs/catch/include/catch.hpp"
 #include "../circular_buffer.hpp"
 
+#include <thread>
+
 #define BUFFER_SIZE 100
 
 TEST_CASE("circular_buffer_test") {
@@ -70,4 +72,43 @@ TEST_CASE("circular_buffer_test") {
         buffer_test.get(test, amount);
         CHECK(amount == BUFFER_SIZE);
     }
+}
+
+TEST_CASE("circular buffer stress test", "[.]") {
+    bestsens::CircularBuffer<int> buffer_test(100000);
+
+    std::array<std::thread, 50> inst_thread;
+    std::array<std::thread, 50> inst_thread_read;
+
+    for(unsigned int i = 0; i < inst_thread.size(); i++) {
+        new (&inst_thread[i]) std::thread([&buffer_test](unsigned int thread_id) {
+            for(int i = 0; i < 1000000; i++) {
+                buffer_test.add(i);
+            }
+        }, i);
+    }
+
+    for(unsigned int i = 0; i < inst_thread_read.size(); i++) {
+        new (&inst_thread_read[i]) std::thread([&buffer_test](unsigned int thread_id) {
+            for(int i = 0; i < 10000; i++) {
+                int amount = 10000;
+                int test[amount];
+                int last_position = 0;
+
+                buffer_test.get(test, amount, last_position);
+            }
+        }, i);
+    }
+
+    std::cout << "threads spawned" << std::endl;
+
+    for(auto& element : inst_thread) {
+        element.join();
+    }
+    std::cout << inst_thread.size() << "M data points added" << std::endl;
+
+    for(auto& element : inst_thread_read) {
+        element.join();
+    }
+    std::cout << inst_thread_read.size() * 100 << "M data points read" << std::endl;
 }
