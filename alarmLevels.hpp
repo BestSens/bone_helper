80e82354f94e3831c5f2a61bd8a89e666ccc758e
Jan Schöppach <jan.schoeppach@bestsens.de>
2018-10-22 15:03:21 +0200

    Add alarmLevels.hpp to define alarm levels globally

---
+++ alarmLevels.hpp
@@ -0,0 +1,16 @@
+/*
+ * alarmLevels.hpp
+ *
+ *  Created on: 22.10.2018
+ *	  Author: Jan Schöppach
+ */
+
+#define ALARM_LEVEL_ON_ERROR	-1
+#define ALARM_LEVEL_NOT_FOUND	-1
+#define ALARM_LEVEL_HH			100
+#define ALARM_LEVEL_H			50
+#define ALARM_LEVEL_PH			25
+#define ALARM_LEVEL_NONE		0
+#define ALARM_LEVEL_PL			-25
+#define ALARM_LEVEL_L			-50
+#define ALARM_LEVEL_LL			-100
