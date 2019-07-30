1229ac5e1bfab8806fbfe3ae5035e9b7ef103420
Jan Schöppach <jan.schoeppach@bestsens.de>
2019-07-30 09:54:47 +0200

    Rework jsonHelper is_json_* functions this requires at least nlohmann/json 3.6.0

--- jsonHelper.hpp
+++ jsonHelper.hpp
@@ -45,28 +45,28 @@ namespace bestsens {
 		}
 	}
 
-	inline bool is_json_number(const json& input, std::string key) {
-		return (input != NULL && input.count(key) > 0 && input.at(key).is_number());
+	inline bool is_json_number(const json& input, const std::string& key) {
+		return (input.contains(key) && input.at(key).is_number());
 	}
 
-	inline bool is_json_array(const json& input, std::string key) {
-		return (input != NULL && input.count(key) > 0 && input.at(key).is_array());
+	inline bool is_json_array(const json& input, const std::string& key) {
+		return (input.contains(key) && input.at(key).is_array());
 	}
 
-	inline bool is_json_string(const json& input, std::string key) {
-		return (input != NULL && input.count(key) > 0 && input.at(key).is_string());
+	inline bool is_json_string(const json& input, const std::string& key) {
+		return (input.contains(key) && input.at(key).is_string());
 	}
 
-	inline bool is_json_bool(const json& input, std::string key) {
-		return (input != NULL && input.count(key) > 0 && input.at(key).is_boolean());
+	inline bool is_json_bool(const json& input, const std::string& key) {
+		return (input.contains(key) && input.at(key).is_boolean());
 	}
 
-	inline bool is_json_object(const json& input, std::string key) {
-		return (input != NULL && input.count(key) > 0 && input.at(key).is_object());
+	inline bool is_json_object(const json& input, const std::string& key) {
+		return (input.contains(key) && input.at(key).is_object());
 	}
 
-	inline bool is_json_node(const json& input, std::string key) {
-		return (input != NULL && input.count(key) > 0 && !input.at(key).is_null());
+	inline bool is_json_node(const json& input, const std::string& key) {
+		return (input.contains(key) && !input.at(key).is_null());
 	}
 
 	inline bool checkedUpdateFromJSON(const json& j, const std::string& name, int& value) {
