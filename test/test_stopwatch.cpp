40c9060d9ef1ce9c0888c18cb81c7d51671f8b37
Markus Jankowski <Markus.Jankowski@bestsens.de>
2020-08-13 12:40:01 +0200

    added stopwatch test

---
+++ test/test_stopwatch.cpp
@@ -0,0 +1,12 @@
+#include "libs/catch/include/catch.hpp"
+#include "../stopwatch.hpp"
+#include <thread>
+
+using namespace bestsens;
+
+TEST_CASE("stopwatch_test") {
+	Stopwatch sw;
+	std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	auto elapsed = sw.elapsed();
+	CHECK(elapsed >= 0.1f);
+}
