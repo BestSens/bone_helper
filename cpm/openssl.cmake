75653441daed06b7402f7289de7dc91049ec58c2
Jan Schöppach <jan.schoeppach@bestsens.de>
2022-11-14 17:27:47 +0100

    Fix openssl file

--- cpm/openssl.cmake
+++ cpm/openssl.cmake
@@ -2,7 +2,8 @@ include(CPM)
 CPMAddPackage(
   NAME openssl
   VERSION 1.1.1n
-  URL https://github.com/janbar/openssl-cmake.git
+  URL https://github.com/janbar/openssl-cmake/archive/refs/tags/1.1.1n-20220327.tar.gz
+  URL_HASH MD5=66e1540ee127ee15feb3aaa85ea4673b
   OVERRIDE_FIND_PACKAGE
 )
 
