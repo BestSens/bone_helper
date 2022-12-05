7e00d8390f0368a454c71cd64b64b49950c9b54b
Jan Schöppach <jan.schoeppach@bestsens.de>
2022-12-05 10:21:43 +0100

    Add check to value_ig_type if input json is an object this avoids an unneccessary exception

--- include/bone_helper/jsonHelper.hpp
+++ include/bone_helper/jsonHelper.hpp
@@ -115,6 +115,10 @@ namespace bestsens {
 
 	template <typename keytype, typename T>
 	auto value_ig_type(const nlohmann::json& input, const keytype& key, const T& default_value) -> T {
+		if (!input.is_object()) {
+			return default_value;
+		}
+		
 		try {
 			return input.value(key, default_value);
 		} catch (const nlohmann::json::type_error& e) {
