6dc22699c913fa31387d770846fd3985fc127ce6
Jan Schöppach <jan.schoeppach@bestsens.de>
2022-12-08 21:27:59 +0100

    Use correctly installed boost library

--- cmake/arm-poky-linux-gnueabi.cmake
+++ cmake/arm-poky-linux-gnueabi.cmake
@@ -49,6 +49,7 @@ set(CMAKE_SYSROOT "/opt/boneos/2.0.3/sysroots/cortexa9hf-vfp-neon-poky-linux-gnu
 set(ENV{SDKTARGETSYSROOT} ${CMAKE_SYSROOT})
 set(ENV{PKG_CONFIG_SYSROOT_DIR} ${CMAKE_SYSROOT})
 set(ENV{PKG_CONFIG_PATH} ${CMAKE_SYSROOT}/usr/lib/pkgconfig)
+set(CMAKE_MODULE_PATH ${CMAKE_SYSROOT}/usr/lib/cmake)
 
 add_compile_options("-march=armv7-a;-mfloat-abi=hard;-mfpu=neon;-mtune=cortex-a9;-pthread")
 add_link_options("-march=armv7-a;-mfloat-abi=hard;-mfpu=neon;-mtune=cortex-a9;-pthread")
---
+++ scripts/install_boost.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+mkdir -p /tmp/boost_1_80_0
+cd /tmp/boost_1_80_0
+wget https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.gz
+tar xzf boost_1_80_0.tar.gz
+cd boost_1_80_0
+./bootstrap.sh || exit 1
+sudo ./b2 install || exit 1
+cd /tmp
+sudo rm -Rf boost_1_80_0
