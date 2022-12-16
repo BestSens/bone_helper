e59aadf1ba135c39ca4e78008f148bc73d19ff66
Jan Schöppach <jan.schoeppach@bestsens.de>
2022-12-16 09:21:14 +0100

    Update boost install script to install version 1.81.0

--- scripts/install_boost.sh
+++ scripts/install_boost.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-mkdir -p /tmp/boost_1_80_0
-cd /tmp/boost_1_80_0
-wget https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.gz
-tar xzf boost_1_80_0.tar.gz
-cd boost_1_80_0
+mkdir -p /tmp/boost_1_81_0
+cd /tmp/boost_1_81_0
+wget https://boostorg.jfrog.io/artifactory/main/release/1.81.0/source/boost_1_81_0.tar.gz
+tar xzf boost_1_81_0.tar.gz
+cd boost_1_81_0
 ./bootstrap.sh || exit 1
 sudo ./b2 install || exit 1
 cd /tmp
-sudo rm -Rf boost_1_80_0
+sudo rm -Rf boost_1_81_0
