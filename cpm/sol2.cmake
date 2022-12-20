4108235739788adf774ba8f61feede460f1b0eed
Jan Schöppach <jan.schoeppach@bestsens.de>
2022-12-20 14:22:18 +0100

    Update sol2 to v3.3.0

--- cpm/sol2.cmake
+++ cpm/sol2.cmake
@@ -1,9 +1,9 @@
 include(CPM)
 CPMAddPackage(
   NAME sol2
-  VERSION 2.20.6
-  URL https://github.com/ThePhD/sol2/archive/refs/tags/v2.20.6.tar.gz
-  URL_HASH MD5=f515a1e7aa65087ebcfd3c343f4b3b34
+  VERSION 3.3.0
+  URL https://github.com/ThePhD/sol2/archive/refs/tags/v3.3.0.tar.gz
+  URL_HASH MD5=05021725f7a3e0b91e19250d001deb8e
   OVERRIDE_FIND_PACKAGE
 )
 
