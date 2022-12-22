ae666b61c209c7f8c23c6a815bdf0ea99f4f6fee
Jan Schöppach <jan.schoeppach@bestsens.de>
2022-12-22 11:43:08 +0100

    Add libcoro cmake file

---
+++ cpm/libcoro.cmake
@@ -0,0 +1,13 @@
+include(CPM)
+CPMAddPackage(
+  NAME libcoro
+  VERSION 0.6
+  OPTIONS
+	"LIBCORO_BUILD_TESTS OFF"
+	"LIBCORO_BUILD_EXAMPLES OFF"
+  GIT_REPOSITORY https://github.com/jbaldwin/libcoro.git
+  GIT_TAG v0.6
+  OVERRIDE_FIND_PACKAGE
+)
+
+find_package(libcoro)
