fcb95aad530f38228bbf40d455d03feffbc85456
Jan Schöppach <jan.schoeppach@bestsens.de>
2022-12-23 15:56:05 +0100

    Make loopTimer header only again with static inline

--- include/bone_helper/loopTimer.hpp
+++ include/bone_helper/loopTimer.hpp
@@ -8,36 +8,68 @@
 #ifndef LOOPTIMER_HPP_
 #define LOOPTIMER_HPP_
 
+#include <atomic>
 #include <chrono>
 #include <condition_variable>
 #include <mutex>
-#include <atomic>
 
 namespace bestsens {
 	class loopTimer {
 	public:
-		explicit loopTimer(std::chrono::microseconds wait_time, bool start_value = false);
-		loopTimer(int wait_time_ms, bool start_value) : loopTimer(std::chrono::milliseconds(wait_time_ms), start_value) {};
-		~loopTimer();
+		loopTimer(std::chrono::microseconds wait_time, bool start_value = false) : wait_time{wait_time} {
+			this->next_cycle =
+				start_value ? std::chrono::steady_clock::now() : std::chrono::steady_clock::now() + this->wait_time;
+		}
 
-		void wait_on_tick();
+		loopTimer(int wait_time_ms, bool start_value)
+			: loopTimer(std::chrono::milliseconds(wait_time_ms), start_value){};
+		~loopTimer() {
+			this->stop();
+		}
 
-		static void kill_all();
+		void wait_on_tick() {
+			do {
+				std::unique_lock<std::mutex> lk(loopTimer::m_trigger);
+				loopTimer::cv_trigger.wait_until(lk, this->next_cycle);
+			} while (!loopTimer::kill && this->running && std::chrono::steady_clock::now() < this->next_cycle);
 
-		auto set_wait_time(std::chrono::microseconds wait_time) -> void;
-		auto get_wait_time() -> std::chrono::microseconds;
+			this->next_cycle = std::chrono::steady_clock::now() + this->wait_time;
+		}
+
+		static void kill_all() {
+			{
+				const std::lock_guard<std::mutex> lk(loopTimer::m_trigger);
+				loopTimer::kill = true;
+			}
+			loopTimer::cv_trigger.notify_all();
+		};
+
+		auto set_wait_time(std::chrono::microseconds wait_time) -> void {
+			this->wait_time = wait_time;
+		}
+
+		auto get_wait_time() -> std::chrono::microseconds {
+			return this->wait_time;
+		}
+
 	private:
 		std::chrono::steady_clock::time_point next_cycle{};
 		std::chrono::microseconds wait_time;
 
 		std::atomic<bool> running{false};
 
-		static std::atomic<bool> kill;
-		static std::condition_variable cv_trigger;
-		static std::mutex m_trigger;
+		static inline std::atomic<bool> kill{false};
+		static inline std::condition_variable cv_trigger{};
+		static inline std::mutex m_trigger{};
 
-		void stop();
+		void stop() {
+			{
+				const std::lock_guard<std::mutex> lk(loopTimer::m_trigger);
+				this->running = false;
+			}
+			loopTimer::cv_trigger.notify_all();
+		}
 	};
-} // namespace bestsens
+}  // namespace bestsens
 
 #endif /* LOOPTIMER_HPP_ */
