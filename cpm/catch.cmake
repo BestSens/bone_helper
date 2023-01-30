6aebab031d9b692caf46f20d8abe222bddb71097
Jan Schöppach <jan.schoeppach@bestsens.de>
2023-01-30 11:20:50 +0100

    Update catch to v3.3.1

--- cpm/catch.cmake
+++ cpm/catch.cmake
@@ -1,9 +1,9 @@
 include(CPM)
 CPMAddPackage(
   NAME catch
-  VERSION 3.3.0
-  URL https://github.com/catchorg/Catch2/archive/refs/tags/v3.3.0.tar.gz
-  URL_HASH MD5=fc7a0ea4b83ceed9e4d1be672d529a73
+  VERSION 3.3.1
+  URL https://github.com/catchorg/Catch2/archive/refs/tags/v3.3.1.tar.gz
+  URL_HASH MD5=5cdc99f93e0b709936eb5af973df2a5c
   OVERRIDE_FIND_PACKAGE
 )
 
