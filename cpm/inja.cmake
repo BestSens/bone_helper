80501905ab35a6bd9e6db75933c7dbc13a31d0f1
Jan Schöppach <jan.schoeppach@bestsens.de>
2023-03-01 10:59:38 +0100

    Add inja cpm configuration

---
+++ cpm/inja.cmake
@@ -0,0 +1,14 @@
+include(CPM)
+CPMAddPackage(
+  NAME inja
+  VERSION 3.4
+  OPTIONS
+    "INJA_USE_EMBEDDED_JSON OFF"
+    "BUILD_TESTING OFF"
+    "BUILD_BENCHMARK OFF"
+  URL https://github.com/pantor/inja/archive/refs/tags/v3.4.0.tar.gz
+  URL_HASH MD5=ccb2e98630334f94faa6781d4c54a1ff
+  OVERRIDE_FIND_PACKAGE
+)
+
+find_package(inja)
