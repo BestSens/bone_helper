445db35ea93a6383b72dfbd86f6afbdeffc90800
Jan Schöppach <jan.schoeppach@bestsens.de>
2023-07-11 09:44:54 +0200

    Update cxxopts to v3.1.1

--- cpm/add_cxxopts.cmake
+++ cpm/add_cxxopts.cmake
@@ -1,12 +1,12 @@
 include(CPM)
 CPMAddPackage(
   NAME cxxopts
-  VERSION 3.0.0
+  VERSION 3.1.1
   OPTIONS
     "CXXOPTS_BUILD_EXAMPLES OFF"
     "CXXOPTS_BUILD_TESTS OFF"
-  URL https://github.com/jarro2783/cxxopts/archive/refs/tags/v3.0.0.tar.gz
-  URL_HASH MD5=4c4cb6e2f252157d096fe18451ab451e
+  URL https://github.com/jarro2783/cxxopts/archive/refs/tags/v3.1.1.tar.gz
+  URL_HASH MD5=61b8bf0d8ab97fd55d67c7c25687b26d
   OVERRIDE_FIND_PACKAGE
 )
 
