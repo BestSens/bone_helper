518aa183dcf5c309877c1bafa5404be375d7db4b
Jan Schöppach <jan.schoeppach@bestsens.de>
2023-11-28 14:18:44 +0100

    Update CPM and paho mqtt

--- cpm/paho_mqttpp.cmake
+++ cpm/paho_mqttpp.cmake
@@ -1,8 +1,9 @@
 include(CPM)
 CPMAddPackage(
 	NAME paho_mqtt_c
-	VERSION 1.3.1
-	URL https://github.com/eclipse/paho.mqtt.c/archive/refs/tags/v1.3.12.tar.gz
+	VERSION 1.3.13
+	URL https://github.com/eclipse/paho.mqtt.c/archive/refs/tags/v1.3.13.tar.gz
+	URL_HASH MD5=74aee81a2620207bae4f26fc641048de
 	OPTIONS
 		"PAHO_WITH_SSL ON"
 		"PAHO_BUILD_SHARED OFF"
@@ -13,7 +14,6 @@ CPMAddPackage(
 		"OPENSSL_CRYPTO_LIBRARY ${OPENSSL_CRYPTO_LIBRARY}"
 		"OPENSSL_SSL_LIBRARY ${OPENSSL_SSL_LIBRARY}"
 		"CMAKE_CXX_STANDARD 11"
-	URL_HASH MD5=b654db416d19931bd3362f26ea9433b3
 )
 
 set(paho_mqtt_c_LIBRARIES "${paho_mqtt_c_BINARY_DIR}/src/libpaho-mqtt3as.a")
@@ -21,8 +21,9 @@ set(paho_mqtt_c_INCLUDE_DIRS "${paho_mqtt_c_SOURCE_DIR}/src/")
 
 CPMAddPackage(
 	NAME paho_mqttpp
-	VERSION 1.2.0
-	URL https://github.com/eclipse/paho.mqtt.cpp/archive/refs/tags/v1.2.0.tar.gz
+	VERSION 1.3.1
+	URL https://github.com/eclipse/paho.mqtt.cpp/archive/refs/tags/v1.3.1.tar.gz
+	URL_HASH MD5=c70e81c1ec15ef369a4620b515b32c60
 	OPTIONS
 		"PAHO_WITH_SSL ON"
 		"PAHO_BUILD_STATIC ON"
@@ -33,7 +34,6 @@ CPMAddPackage(
 		"PAHO_MQTT_C_LIBRARIES ${paho_mqtt_c_LIBRARIES}"
 		"PAHO_MQTT_C_INCLUDE_DIRS ${paho_mqtt_c_INCLUDE_DIRS}"
 		"CMAKE_CXX_STANDARD 11"
-	URL_HASH MD5=b654db416d19931bd3362f26ea9433b3
 )
 
 set(paho_mqttpp_LIBRARIES "${paho_mqttpp_BINARY_DIR}/src/libpaho-mqttpp3.a")
