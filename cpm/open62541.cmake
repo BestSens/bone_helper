58e12de0cd9c6fd6d440670bb1ac8a566e814843
Jan Schöppach <jan.schoeppach@bestsens.de>
2023-12-08 15:14:24 +0100

    Add CPM file for open62541

---
+++ cpm/open62541.cmake
@@ -0,0 +1,15 @@
+include(CPM)
+CPMAddPackage(
+  NAME open62541
+  VERSION 1.3.9
+  URL https://github.com/open62541/open62541/archive/refs/tags/v1.3.9.zip
+  URL_HASH MD5=5778a4c3fbca52e8bdc94a7f3c6d0f63
+  OVERRIDE_FIND_PACKAGE
+  OPTIONS
+  	"UA_MULTITHREADING 100"
+	"UA_ENABLE_DISCOVERY ON"
+	"UA_ENABLE_ENCRYPTION ON"
+	"UA_ENABLE_ENCRYPTION_OPENSSL ON"
+)
+
+find_package(open62541)
