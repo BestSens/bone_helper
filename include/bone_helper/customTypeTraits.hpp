63e3dcf29f44fc0871d671f94ab7382a179d8a41
Jan Schöppach <jan.schoeppach@bestsens.de>
2024-02-22 14:56:52 +0100

    Add custom type traits

---
+++ include/bone_helper/customTypeTraits.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <expected>
+#include <limits>
+
+enum class cast_error { negative_overflow, positive_overflow };
+
+template <class T, class I>
+constexpr auto safeCast(const I in) -> std::expected<T, cast_error> {
+	if (std::cmp_less(in, std::numeric_limits<T>::min())) {
+		return std::unexpected(cast_error::negative_overflow);
+	}
+
+	if (std::cmp_greater(in, std::numeric_limits<T>::max())) {
+		return std::unexpected(cast_error::positive_overflow);
+	}
+
+	return static_cast<T>(in);
+}
+
+template <class T, class I>
+constexpr auto coerceCast(const I in) -> T {
+	const auto ret = safeCast<T>(in);
+
+	if (ret) {
+		return *ret;
+	}
+
+	if (ret.error() == cast_error::negative_overflow) {
+		return std::numeric_limits<T>::min();
+	} else {
+		return std::numeric_limits<T>::max();
+	}
+}
