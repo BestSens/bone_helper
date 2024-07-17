b2b58a776876ebda9efd66ea0f2c96dcda730bb7
Jan Schöppach <jan.schoeppach@bestsens.de>
2024-07-17 17:31:33 +0200

    Update open62541 to v 1.4.2

--- cpm/open62541.cmake
+++ cpm/open62541.cmake
@@ -1,9 +1,9 @@
 include(CPM)
 CPMAddPackage(
   NAME open62541
-  VERSION 1.3.9
-  URL https://github.com/open62541/open62541/archive/refs/tags/v1.3.9.zip
-  URL_HASH MD5=5778a4c3fbca52e8bdc94a7f3c6d0f63
+  VERSION 1.4.2
+  URL https://github.com/open62541/open62541/archive/refs/tags/v1.4.2.zip
+  URL_HASH MD5=e963b05b75c6f3b9b5f8e27a863ed079
   OVERRIDE_FIND_PACKAGE
   OPTIONS
   	"UA_MULTITHREADING 100"
