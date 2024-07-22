37010d47413b9c219a68cbfc47cb10feab5810f5
Jan Schöppach <jan.schoeppach@bestsens.de>
2024-07-22 10:25:55 +0200

    Update fmt to v11.0.2

--- cpm/fmt.cmake
+++ cpm/fmt.cmake
@@ -1,9 +1,9 @@
 include(CPM)
 CPMAddPackage(
   NAME fmt
-  VERSION 11.0.1
-  URL https://github.com/fmtlib/fmt/releases/download/11.0.1/fmt-11.0.1.zip
-  URL_HASH MD5=5f3915e2eff60e7f70c558120592100d
+  VERSION 11.0.2
+  URL https://github.com/fmtlib/fmt/releases/download/11.0.2/fmt-11.0.2.zip
+  URL_HASH MD5=c622dca45ec3fc95254c48370a9f7a1d
   OVERRIDE_FIND_PACKAGE
 )
 
