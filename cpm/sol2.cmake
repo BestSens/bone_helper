0a93f233768f14db9c9d88ac676777c18a52b322
Jan Schöppach <jan.schoeppach@bestsens.de>
2025-04-14 13:18:12 +0200

    Fix sol2 hash

--- cpm/sol2.cmake
+++ cpm/sol2.cmake
@@ -3,5 +3,5 @@ CPMAddPackage(
   NAME sol2
   VERSION 3.3.0
   URL https://github.com/ThePhD/sol2/archive/refs/tags/v3.3.0.tar.gz
-  URL_HASH SHA256=05021725f7a3e0b91e19250d001deb8e
+  URL_HASH SHA256=b82c5de030e18cb2bcbcefcd5f45afd526920c517a96413f0b59b4332d752a1e
 )
