d1c14964b4678d38bb20202835c19a54c6c6bdd7
Jan Schöppach <jan.schoeppach@bestsens.de>
2025-04-14 13:06:58 +0200

    Update catch2 to v3.8.1

--- cpm/catch.cmake
+++ cpm/catch.cmake
@@ -1,9 +1,9 @@
 include(CPM)
 CPMAddPackage(
   NAME catch
-  VERSION 3.7.1
-  URL https://github.com/catchorg/Catch2/archive/refs/tags/v3.7.1.tar.gz
-  URL_HASH MD5=9fcbec1dc95edcb31c6a0d6c5320e098
+  VERSION 3.8.1
+  URL https://github.com/catchorg/Catch2/archive/refs/tags/v3.8.1.tar.gz
+  URL_HASH SHA256=18b3f70ac80fccc340d8c6ff0f339b2ae64944782f8d2fca2bd705cf47cadb79
   OVERRIDE_FIND_PACKAGE
 )
 
