6545044c5afd458e2b6f322146c8b96ea2577b0a
Jan Schöppach <jan.schoeppach@metax-gmbh.de>
2025-06-24 11:21:35 +0200

    Update paho-mqttpp to v1.5.3

--- cpm/paho_mqttpp.cmake
+++ cpm/paho_mqttpp.cmake
@@ -1,6 +1,6 @@
 include(CPM)
 CPMAddPackage(
-	NAME paho_mqtt_c
+	NAME eclipse-paho-mqtt-c
 	VERSION 1.3.14
 	URL https://github.com/eclipse/paho.mqtt.c/archive/refs/tags/v1.3.14.tar.gz
 	URL_HASH SHA256=7af7d906e60a696a80f1b7c2bd7d6eb164aaad908ff4c40c3332ac2006d07346
@@ -15,14 +15,15 @@ CPMAddPackage(
 		"OPENSSL_SSL_LIBRARY ${OPENSSL_SSL_LIBRARY}"
 )
 
-set(paho_mqtt_c_LIBRARIES "${paho_mqtt_c_BINARY_DIR}/src/libpaho-mqtt3as.a")
-set(paho_mqtt_c_INCLUDE_DIRS "${paho_mqtt_c_SOURCE_DIR}/src")
+if(TARGET paho-mqtt3as-static)
+    export(TARGETS paho-mqtt3as-static NAMESPACE eclipse-paho-mqtt-c:: FILE "${eclipse-paho-mqtt-c_BINARY_DIR}/PahoMqttCStaticTargets.cmake")
+endif()
 
 CPMAddPackage(
-	NAME paho_mqttpp
-	VERSION 1.3.2
-	GIT_REPOSITORY https://github.com/eclipse/paho.mqtt.cpp.git
-	GIT_TAG v1.3.2
+	NAME paho-mqttpp
+	VERSION 1.5.3
+	URL https://github.com/eclipse/paho.mqtt.cpp/archive/refs/tags/v1.5.3.tar.gz
+	URL_HASH SHA256=8aab7761bcb43e2d65dbf266c8623d345f7612411363a97aa66370fb9822d0b9
 	OPTIONS
 		"PAHO_WITH_SSL ON"
 		"PAHO_BUILD_STATIC ON"
@@ -30,9 +31,7 @@ CPMAddPackage(
 		"PAHO_ENABLE_TESTING OFF"
 		"PAHO_BUILD_SAMPLES OFF"
 		"PAHO_BUILD_DOCUMENTATION OFF"
-		"PAHO_MQTT_C_LIBRARIES ${paho_mqtt_c_LIBRARIES}"
-		"PAHO_MQTT_C_INCLUDE_DIRS ${paho_mqtt_c_INCLUDE_DIRS}"
 )
 
-set(paho_mqttpp_LIBRARIES "${paho_mqttpp_BINARY_DIR}/src/libpaho-mqttpp3.a")
-set(paho_mqttpp_INCLUDE_DIRS "${paho_mqttpp_SOURCE_DIR}/src")
+# set(paho_mqttpp_LIBRARIES "${paho_mqttpp_BINARY_DIR}/src/libpaho-mqttpp3.a")
+# set(paho_mqttpp_INCLUDE_DIRS "${paho_mqttpp_SOURCE_DIR}/include")
